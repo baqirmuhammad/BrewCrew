a36975e809b9802cca436eac99b53e6c7054981e
baqirmuhammad <bmuhammad.bscs20seecs@seecs.edu.pk>
2023-07-06 17:15:17 +0500

    added slider in settings

--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -11,9 +11,9 @@ class SettingsForm extends StatefulWidget {
 class _SettingsFormState extends State<SettingsForm> {
   final _formkey = GlobalKey<FormState>();
   final List<String> sugars = ['0', '1', '2', '3', '4'];
-  late String _currentName;
-  late String _currentSugars;
-  late int _currentStrength;
+  String? _currentName;
+  String? _currentSugars;
+  int? _currentStrength;
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -33,14 +33,24 @@ class _SettingsFormState extends State<SettingsForm> {
           const SizedBox(height: 20.0),
           //dropdown
           DropdownButtonFormField(
+            value: _currentSugars ?? '0',
             decoration: textInputDecoration,
             items: sugars.map((sugar) {
               return DropdownMenuItem(
                   value: sugar, child: Text('$sugar sugars'));
             }).toList(),
-            onChanged: (val) => setState(() => _currentSugars = val!),
+            onChanged: (val) => setState(() => _currentSugars = val),
           ),
           //slider
+          Slider(
+            value: (_currentStrength ?? 100).toDouble(),
+            activeColor: Colors.brown[_currentStrength ?? 100],
+            inactiveColor: Colors.brown[_currentStrength ?? 100],
+            min: 100,
+            max: 900,
+            divisions: 8,
+            onChanged: (val) => setState(() => _currentStrength = val.round()),
+          ),
           //button
           ElevatedButton(
               style: ElevatedButton.styleFrom(
